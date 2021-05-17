fda462fc64f6363786c961e37308e7b4e9394e28
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-05-17 19:31:20 +0900

    add test

--- example/client-node/tests/datachannel.test.ts
+++ example/client-node/tests/datachannel.test.ts
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import { Client } from "../src/client";
 import { socketPromise } from "../src/socket.io-promise";
-import { waitFor } from "./fixture";
+import { Counter, waitFor } from "./fixture";
 
 describe("datachannel", () => {
   test("produce(data) consume", async (done) => {
@@ -27,5 +27,44 @@ describe("datachannel", () => {
     setInterval(() => {
       producer.send("data");
     }, 100);
+  });
+
+  test("produce(data) produce(data) consume consume", async (done) => {
+    const socket = io.connect("http://127.0.0.1:20000");
+
+    await socketPromise(socket)("join");
+
+    const client = await new Client(socket).init();
+    await client.setupConsumerTransport();
+    await client.setupProducerTransport();
+
+    const counter = new Counter(2, async () => {
+      socket.close();
+      await new Promise((r) => setTimeout(r, waitFor));
+      done();
+    });
+
+    let index = 1;
+    client.onProduceData.subscribe(async (target) => {
+      const toBe = index++;
+      const consumer = await client.consumeData(target);
+      consumer.on("message", async (data) => {
+        expect(data).toBe(toBe.toString());
+        counter.done();
+      });
+    });
+
+    {
+      const producer = await client.publishData();
+      setInterval(() => {
+        producer.send("1");
+      }, 100);
+    }
+    {
+      const producer = await client.publishData();
+      setInterval(() => {
+        producer.send("2");
+      }, 100);
+    }
   });
 });

7ae16f92f28116585d681877441696c94b29a029
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-05-19 20:17:15 +0900

    wip

--- example/client-node/src/media.ts
+++ example/client-node/src/media.ts
@@ -1,7 +1,19 @@
 import io from "socket.io-client";
 import { createSocket } from "dgram";
 import { Client } from "./client";
-import { MediaStreamTrack } from "../../../src";
+import {
+  MediaStreamTrack,
+  RTCRtpCodecParameters,
+  useAbsSendTime,
+  useFIR,
+  useNACK,
+  usePLI,
+  useREMB,
+  useSdesMid,
+  useTransportWideCC,
+  useTWCC,
+} from "../../../src";
+import { socketPromise } from "./socket.io-promise";
 
 // ffmpeg -re -f lavfi -i testsrc=size=640x480:rate=30 -vcodec libvpx -cpu-used 5 -deadline 1 -g 10 -error-resilient 1 -auto-alt-ref 1 -f rtp rtp://127.0.0.1:5000
 // gst-launch-1.0 videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000
@@ -15,11 +27,24 @@ console.log("start");
 socket.on("connect", async () => {
   console.log("connected");
 
-  socket.emit("join");
-  await new Promise((r) => socket.on("join", r));
+  await socketPromise(socket)("join");
   console.log("joined");
 
-  const client = await new Client(socket).init();
+  const client = await new Client(socket, {
+    headerExtensions: {
+      video: [useSdesMid(), useAbsSendTime(), useTransportWideCC()],
+    },
+    codecs: {
+      video: [
+        new RTCRtpCodecParameters({
+          mimeType: "video/VP8",
+          clockRate: 90000,
+          payloadType: 98,
+          rtcpFeedback: [useFIR(), useNACK(), usePLI(), useREMB(), useTWCC()],
+        }),
+      ],
+    },
+  }).init();
   await client.setupConsumerTransport();
   await client.setupProducerTransport();
 
